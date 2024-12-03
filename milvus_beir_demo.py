dd3854d8e92959518e45158e7bfa2e0ea164c345
zhuwenxing <wenxing.zhu@zilliz.com>
2024-12-03 17:28:28 +0800

    update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- milvus_beir_demo.py
+++ milvus_beir_demo.py
@@ -6,7 +6,7 @@ from milvus_beir.retrieval.search.dense.dense_search import MilvusDenseSearch
 from milvus_beir.retrieval.search.sparse.sparse_search import MilvusSparseSearch
 from milvus_beir.retrieval.search.lexical.multi_match_search import MilvusMultiMatchSearch
 from milvus_beir.retrieval.search.lexical.bm25_search import MilvusBM25Search
-
+from milvus_beir.retrieval.search.hybrid.bm25_hybrid_search import MilvusBM25DenseHybridSearch
 
 from beir import util
 from beir.datasets.data_loader import GenericDataLoader
@@ -32,7 +32,18 @@ milvus_client = MilvusClient(uri="http://10.104.20.192:19530")
 # print("Precision:", precision)
 
 
-model = MilvusSparseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+# model = MilvusSparseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+# retriever = EvaluateRetrieval(model)
+# results = retriever.retrieve(corpus, queries)
+# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+# print("NDCG:", ndcg)
+# print("MAP:", _map)
+# print("Recall:", recall)
+# print("Precision:", precision)
+
+
+
+model = MilvusBM25DenseHybridSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
 retriever = EvaluateRetrieval(model)
 results = retriever.retrieve(corpus, queries)
 ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
