7d64b6e06e9552bd08873c0c40e378dc870d3268
zhuwenxing <wenxing.zhu@zilliz.com>
2024-12-03 19:09:31 +0800

    update
    Signed-off-by: zhuwenxing <wenxing.zhu@zilliz.com>

--- milvus_beir_demo.py
+++ milvus_beir_demo.py
@@ -13,6 +13,8 @@ from beir import util
 from beir.datasets.data_loader import GenericDataLoader
 from beir.retrieval.evaluation import EvaluateRetrieval
 
+from ranx import Qrels, Run, compare
+
 dataset = "scifact"
 url = "https://public.ukp.informatik.tu-darmstadt.de/thakur/BEIR/datasets/{}.zip".format(dataset)
 data_path = util.download_and_unzip(url, "datasets")
@@ -23,62 +25,77 @@ print("Corpus:", len(corpus))
 print("Queries:", len(queries))
 
 milvus_client = MilvusClient(uri="http://10.104.20.192:19530")
-# model = MilvusDenseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
-# retriever = EvaluateRetrieval(model)
-# results = retriever.retrieve(corpus, queries)
-# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-# print("NDCG:", ndcg)
-# print("MAP:", _map)
-# print("Recall:", recall)
-# print("Precision:", precision)
-
-
-# model = MilvusSparseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
-# retriever = EvaluateRetrieval(model)
-# results = retriever.retrieve(corpus, queries)
-# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-# print("NDCG:", ndcg)
-# print("MAP:", _map)
-# print("Recall:", recall)
-# print("Precision:", precision)
-
-
-
-# model = MilvusBM25DenseHybridSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
-# retriever = EvaluateRetrieval(model)
-# results = retriever.retrieve(corpus, queries)
-# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-# print("NDCG:", ndcg)
-# print("MAP:", _map)
-# print("Recall:", recall)
-# print("Precision:", precision)
-
-
-model = MilvusSparseDenseHybridSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+model = MilvusDenseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
 retriever = EvaluateRetrieval(model)
-results = retriever.retrieve(corpus, queries)
-ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
+dense_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, dense_results, retriever.k_values)
 print("NDCG:", ndcg)
 print("MAP:", _map)
 print("Recall:", recall)
 print("Precision:", precision)
 
 
-# model = MilvusMultiMatchSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
-# retriever = EvaluateRetrieval(model)
-# results = retriever.retrieve(corpus, queries)
-# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-# print("NDCG:", ndcg)
-# print("MAP:", _map)
-# print("Recall:", recall)
-# print("Precision:", precision)
+model = MilvusSparseSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+retriever = EvaluateRetrieval(model)
+sparse_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, sparse_results, retriever.k_values)
+print("NDCG:", ndcg)
+print("MAP:", _map)
+print("Recall:", recall)
+print("Precision:", precision)
+
+
+
+model = MilvusBM25DenseHybridSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+retriever = EvaluateRetrieval(model)
+bm25_dense_hybrid_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, bm25_dense_hybrid_results, retriever.k_values)
+print("NDCG:", ndcg)
+print("MAP:", _map)
+print("Recall:", recall)
+print("Precision:", precision)
+
+
+model = MilvusSparseDenseHybridSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+retriever = EvaluateRetrieval(model)
+sparse_dense_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, sparse_dense_results, retriever.k_values)
+print("NDCG:", ndcg)
+print("MAP:", _map)
+print("Recall:", recall)
+print("Precision:", precision)
+
+
+model = MilvusMultiMatchSearch(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+retriever = EvaluateRetrieval(model)
+multi_match_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, multi_match_results, retriever.k_values)
+print("NDCG:", ndcg)
+print("MAP:", _map)
+print("Recall:", recall)
+print("Precision:", precision)
 #
 #
-# model = MilvusBM25Search(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
-# retriever = EvaluateRetrieval(model)
-# results = retriever.retrieve(corpus, queries)
-# ndcg, _map, recall, precision = retriever.evaluate(qrels, results, retriever.k_values)
-# print("NDCG:", ndcg)
-# print("MAP:", _map)
-# print("Recall:", recall)
-# print("Precision:", precision)
+model = MilvusBM25Search(milvus_client, collection_name="milvus_beir_demo", nq=100, nb=1000)
+retriever = EvaluateRetrieval(model)
+bm25_results = retriever.retrieve(corpus, queries)
+ndcg, _map, recall, precision = retriever.evaluate(qrels, bm25_results, retriever.k_values)
+print("NDCG:", ndcg)
+print("MAP:", _map)
+print("Recall:", recall)
+print("Precision:", precision)
+
+
+qrels = Qrels(qrels)
+run_dense = Run(dense_results,name="dense")
+run_sparse = Run(sparse_results,name="sparse")
+run_bm25 = Run(bm25_results,name="bm25")
+run_multi_match = Run(multi_match_results,name="multi_match")
+run_bm25_dense_hybrid = Run(bm25_dense_hybrid_results,name="bm25_dense_hybrid")
+run_sparse_dense = Run(sparse_dense_results,name="sparse_dense")
+report = compare(
+    qrels=qrels,
+    runs=[run_dense, run_sparse, run_bm25, run_multi_match, run_bm25_dense_hybrid, run_sparse_dense],
+    metrics=["ndcg@10", "map@10", "recall@10", "precision@10"],
+)
+print(report)
